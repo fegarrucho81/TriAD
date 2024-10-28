c5be79d48b264d30f613eb85da73f99820f3dcbb
Felipe Garrucho <165536666+fegarrucho81@users.noreply.github.com>
2024-10-28 15:44:53 -0300

    Corrigir bug do último commit

--- script.js
+++ script.js
@@ -2,14 +2,13 @@
 
 window.onload = function() {
     setTimeout(() => {
-        document.getElementById('splashLogo').style.animation = 'none'; // Para parar a animação
-        document.getElementById('homeLogo').style.display = 'block'; // Exibir o logo na home
-        document.getElementById('homeScreen').style.display = 'block'; // Exibir a tela de home
-        document.querySelector('.splash-screen').style.display = 'none'; // Esconder a tela de splash
-        // Exibir o texto e o conteúdo da tela de home
+        document.getElementById('splashLogo').style.animation = 'none';
+        document.getElementById('homeLogo').style.display = 'block';
+        document.getElementById('homeScreen').style.display = 'block';
+        document.querySelector('.splash-screen').style.display = 'none';
         document.getElementById('homeText').style.display = 'block';
         document.querySelector('.button-container').style.display = 'flex';
-    }, 2000); // Tempo da animação do logo (2s)
+    }, 2000);
 }
 
 function toggleConfigCard() {
@@ -25,21 +24,9 @@ window.addEventListener("load", function () {
     setTimeout(function () {
         document.getElementById("splashScreen").style.display = "none";
         document.getElementById("homeScreen").style.display = "block";
-    }, 2000); // Ajuste o tempo, em milissegundos
+    }, 2000);
 });
 
-function endGame() {
-    // Adiciona um delay de 500ms antes de exibir a tela final
-    setTimeout(() => {
-      showFinalScreen(); // Função que exibe a tela final
-    }, 500);
-  }
-  
-  function showFinalScreen() {
-    // Lógica para exibir a tela final
-    document.getElementById("final-screen").style.display = "block";
-  }
-  
 // Função para alternar o idioma
 function changeLanguage() {
     const language = document.getElementById('language').value;
@@ -57,6 +44,13 @@ function changeLanguage() {
         document.getElementById('aboutTitle').innerText = "About";
         document.getElementById('aboutText').innerText = "Inspired by the BBC TV show Only Connect's Connecting Wall game.";
         document.getElementById('closeAboutButton').innerText = "Close";
+
+        // Traduzindo as opções dos seletores
+        document.getElementById('language').options[0].text = "Portuguese";
+        document.getElementById('language').options[1].text = "English";
+        document.getElementById('sound').options[0].text = "On";
+        document.getElementById('sound').options[1].text = "Off";
+
     } else {
         document.getElementById('homeText').innerText = "Forme 3 grupos de 3 palavras que tenham algo em comum";
         document.getElementById('playButton').innerText = "Jogar";
@@ -70,5 +64,11 @@ function changeLanguage() {
         document.getElementById('aboutTitle').innerText = "Sobre";
         document.getElementById('aboutText').innerText = "Inspirado pelo jogo Connecting Wall do programa de TV da BBC Only Connect.";
         document.getElementById('closeAboutButton').innerText = "Fechar";
+
+        // Traduzindo de volta para português
+        document.getElementById('language').options[0].text = "Português";
+        document.getElementById('language').options[1].text = "Inglês";
+        document.getElementById('sound').options[0].text = "Ativado";
+        document.getElementById('sound').options[1].text = "Desativado";
     }
 }
